d14b020ab60757d6dd784850ae37602e7bafe1de
Shani Mithani <shanimithani@gmail.com>
2023-09-24 12:43:47 -0400

    updated

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,14 +1,6 @@
 class Admin::DashboardController < ApplicationController
-  before_action :http_basic_authenticate
-
   def show
-  end
-
-  private
-
-  def http_basic_authenticate
-    authenticate_or_request_with_http_basic do |username, password|
-      username == ENV['BASIC_AUTH_USERNAME'] && BCrypt::Password.new(ENV['BASIC_AUTH_PASSWORD_HASH']) == password #Authentication from env
-    end
+    @product_count = Product.count
+    @category_count = Category.count
   end
 end
